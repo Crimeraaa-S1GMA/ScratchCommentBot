56adfef2525c803eb597b62e7c73645cce130a38
majkeli122 <67538845+majkeli122@users.noreply.github.com>
2022-11-22 18:26:46 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,32 @@
+import scratchclient
+import random
+import time
+import json
+
+print("  __                        _                               _         ")
+print(" (_   _ ._ _. _|_  _ |_    /   _  ._ _  ._ _   _  ._ _|_   |_)  _ _|_ ")
+print(" __) (_ | (_|  |_ (_ | |   \_ (_) | | | | | | (/_ | | |_   |_) (_) |_ ")
+print("By Crimeraaa")
+print("Warning: I AM NOT RESPONSIBLE FOR ANY BANS FROM THE USAGE OF THIS")
+
+config = open("config.json", "r")
+config_loaded = json.loads(config.read())
+
+comments = open("comments.txt", "r").readlines()
+project_ids = open("projectid.txt", "r").readlines()
+session = scratchclient.ScratchSession(config_loaded["userName"], config_loaded["password"])
+
+session.login(config_loaded["password"])
+
+while True:
+    for p in project_ids:
+        project = session.get_project(p)
+        try:
+            if project.comments_allowed:
+                project.post_comment(
+                    random.choice(comments) + " " + str(random.randint(100, 999)))
+                print("Comment posted!\n\nThere's a 60 second delay + comment posting duration due to Scratch's limits")
+                time.sleep(60)
+        except:
+            pass
+
